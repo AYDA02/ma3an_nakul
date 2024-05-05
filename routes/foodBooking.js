ba16a7a5ba45d497fa1ca1eea80b17130e25852f
AYDA02 <s12029716@stu.najah.edu>
2024-05-05 16:50:18 +0300

    .

--- routes/foodBooking.js
+++ routes/foodBooking.js
@@ -13,7 +13,7 @@ router.post("/submitOrders", (req, res)=>{
     //Check if the user made a real reservation(select at least one to reseve)
     //if not, display an error message 
         if(req.body.menuId.length){
-        db.query("SELECT * FROM luqmataish.menu where account_id =? ", [resturantId], (error, _result)=>{
+        db.query("SELECT * FROM ma3annakul.menu where account_id =? ", [resturantId], (error, _result)=>{
             if(error){
                 console.log("Error while retreiving menu ids     "+error)
                 res.redirect(`/errorPage`)
@@ -23,19 +23,19 @@ router.post("/submitOrders", (req, res)=>{
                 }
 
                 for(let i = 0; i < _result.length; i++){
-                    db.query("select * from luqmataish.orders where account_id =? and menu_id =?", [chrityId, _result[i].id], (error, ordersResult)=>{
+                    db.query("select * from ma3annakul.orders where account_id =? and menu_id =?", [chrityId, _result[i].id], (error, ordersResult)=>{
                         if(error){
                             console.log("Error while retreiving from orders     "+error)
                             res.redirect('/errorPage')             
                         }else{ 
                             if(ordersResult.length == 0){
                                 if(frequency[i] != 0){
-                                    db.query("INSERT INTO `luqmataish`.`orders` (`date`, `time`, `quantity`, `account_id`, `menu_id`) VALUES (?, ?, ?, ?, ?)", [currentTime.toLocaleDateString(), currentTime.toLocaleTimeString(), frequency[i], chrityId, _result[i].id], (error, result)=>{
+                                    db.query("INSERT INTO `ma3annakul`.`orders` (`date`, `time`, `quantity`, `account_id`, `menu_id`) VALUES (?, ?, ?, ?, ?)", [currentTime.toLocaleDateString(), currentTime.toLocaleTimeString(), frequency[i], chrityId, _result[i].id], (error, result)=>{
                                         if(error){
                                             console.log("Error while retreiving from orders     "+error)
                                             res.redirect('/errorPage') 
                                         }else{
-                                            db.query("UPDATE `luqmataish`.`menu` SET quantity = ? WHERE id =?",[_result[i].quantity - frequency[i], _result[i].id], (error, result)=>{
+                                            db.query("UPDATE `ma3annakul`.`menu` SET quantity = ? WHERE id =?",[_result[i].quantity - frequency[i], _result[i].id], (error, result)=>{
                                                 if(error){
                                                     console.log("Error while updating the quantity     "+error)
                                                     res.redirect('/errorPage') 
@@ -46,12 +46,12 @@ router.post("/submitOrders", (req, res)=>{
                                 }
                             }else{
                                 if(frequency[i] != 0){
-                                    db.query("UPDATE `luqmataish`.`orders` SET quantity = ? WHERE id =?",[ordersResult[0].quantity + frequency[i], ordersResult[0].id], (error, result)=>{
+                                    db.query("UPDATE `ma3annakul`.`orders` SET quantity = ? WHERE id =?",[ordersResult[0].quantity + frequency[i], ordersResult[0].id], (error, result)=>{
                                         if(error){
                                             console.log("Error while updating the quantity     "+error)
                                             res.redirect('/errorPage') 
                                         }else{
-                                            db.query("UPDATE `luqmataish`.`menu` SET quantity = ? WHERE id =?",[_result[i].quantity - frequency[i], _result[i].id], (error, result)=>{
+                                            db.query("UPDATE `ma3annakul`.`menu` SET quantity = ? WHERE id =?",[_result[i].quantity - frequency[i], _result[i].id], (error, result)=>{
                                                 if(error){
                                                     console.log("Error while updating the quantity     "+error)
                                                     res.redirect('/errorPage') 
